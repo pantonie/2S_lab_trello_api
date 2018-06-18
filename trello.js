d823aa2f4067a5a45c6ada8db71e64647d8bf9b8
Anton Panteleev <anton@panteleev.net>
2018-06-18 18:58:25 +0300

    Changed request string to simplify url view

--- trello.js
+++ trello.js
@@ -8,22 +8,22 @@ module.exports = class Trello {
         this.token = keys.token;
         this.board = keys.id;
         this.auth = `?key=${this.key}&token=${this.token}`;
+        this.url = url + '{params}'+this.auth;
     }
 
     get(obj, req, res){
         var response;
         switch (obj.name){
             case 'lists': {
-                request({method: 'GET', url: `${url}boards/${this.board}/lists/${this.auth}`}, function (error, _response, body) {
+                request({method: 'GET', url: this.url.toString().replace('{params}',`boards/${this.board}/lists`)}, function (error, _response, body) {
                     if (error) throw new Error(error);
                     response = (JSON.parse(body)).map(function (val) {return {id: val.id, name: val.name}});
                     res.send(response);
-
                 });
                 break;
             }
             case 'list':{
-                request({method: 'GET', url: `${url}lists/${obj.id}/cards${this.auth}`}, function (error, _response, body) {
+                request({method: 'GET', url: this.url.toString().replace('{params}',`lists/${obj.id}/cards`)}, function (error, _response, body) {
                     if (error) throw new Error(error);
                     response = (JSON.parse(body)).map(function(val){return {id: val.id, name: val.name, votes: val.badges.votes}});
                     response.sort(function(a,b){return b.votes - a.votes})
